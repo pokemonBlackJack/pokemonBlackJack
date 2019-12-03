d11d0b81ebcbf59f79b840e098349bc33b8fd47c
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-12-03 18:49:30 -0500

    started creating the function

---
+++ src/getRandomPokemon.js
@@ -0,0 +1,15 @@
+
+const getRandomNumber = () => {
+    const randomNumber = Math.ceil(Math.random() * 100);
+    return randomNumber
+} 
+
+
+function getRandomPokemon() {
+    
+    const randomNumber = getRandomNumber();
+
+
+}
+
+export default getRandomPokemon;
