8a8d5ff6d533848f80222d669938bcbf48fffdf8
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-12-03 21:43:54 -0500

    added the get random pokemon function and tested it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      randomPokemons:[]
+    }
+  }
+
+  componentDidMount() {
+    // Calling the method to get the random pokemons when the app is starting
+    this.getRandomPokemon(2);
+    
+  }
+
+  getRandomPokemon = (numberOfPlayers) => {
+
+    // Foor loop that will repeat depending on the amount of players that will play the game
+
+    for (let i = 0; i < numberOfPlayers; i++){
+
+      // Create an empty object that will store the information about the pokemon that we get randomly
+
+      const pokemons = {};
+      
+      // Getting a random number between 1 and 500 using the random function
+
+      const randomNumber = Math.ceil(Math.random() * 300);
+  
+      // Making an API call using the random number in order to get a random pokemon
+
+      axios({
+        url: `https://pokeapi.co/api/v2/evolution-chain/${randomNumber}/`,
+        method: "get",
+        responseType: "json",
+      }).then((response) => {
+
+        // Storing the part of the response that we need in an array
+        const pokemonEvolutionsArray = response.data.chain.evolves_to;
+  
+        // Check if the pokemon has an evolution, if it doesn't then use recursion to call the function one more time and get a new pokemon.
+
+        if (pokemonEvolutionsArray.length === 0) {
+          this.getRandomPokemon(1);
+        } else {
+
+          // If the pokemon has an evolution then store the base pokemon name and the evolution name in the empty object we created before
+
+          const pokemonName = response.data.chain.species.name;
+          const pokemonNextEvolution = pokemonEvolutionsArray[0].species.name;
+
+          console.log(pokemonName);
+          console.log(pokemonNextEvolution);
+  
+          pokemons.firstPokemon = pokemonName;
+          pokemons.evolution = pokemonNextEvolution;
+          
+          // Make an empty array to store the promises of calling the api to get the base pokemon and the evolution image
+
+          const imagesPromises = [];
+          
+          // Make the api call for both images and store the promise in the array
+
+          const promiseOne = axios({
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+            method: "get",
+            responseType: "json"
+          })
+  
+          imagesPromises.push(promiseOne);
+  
+          const promiseTwo = axios({
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemonNextEvolution}`,
+            method: "get",
+            responseType: "json"
+          })
+  
+          imagesPromises.push(promiseTwo);
+
+          // Use promise.all to wait for both responses before storing them on the state
+
+          Promise.all(imagesPromises).then((response) => {
+
+            // When both promises resolve then get the current pokemons in the state and store them in a variable.
+
+            const currentPokemons = [...this.state.randomPokemons];
+  
+            // Store both images in the object we first created
+
+            pokemons.firstPokemonImg = response[0].data.sprites.front_default;
+  
+            pokemons.evolutionPokemonImg = response[1].data.sprites.front_default;
+
+            // Push the object with all the info into the array that has the current state stored
+
+            currentPokemons.push(pokemons);
+  
+            // Set the state to the new array
+
+            this.setState({
+              randomPokemons: currentPokemons
+            }, () => {
+                
+                // Console login when all of this is done just to see the result
+
+                console.log(this.state.randomPokemons);
+            })
+          })
+        }
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Rendering the pokemons and the names just to see what we are getting */}
+        {this.state.randomPokemons.map((pokemon) => {
+          return <div>
+            <p>{pokemon.firstPokemon}</p>
+            <img src={pokemon.firstPokemonImg} alt="" />
+            <p>{pokemon.evolution}</p>
+            <img src={pokemon.evolutionPokemonImg} alt="" />
+          </div>
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
