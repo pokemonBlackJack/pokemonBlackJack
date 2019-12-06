957df8f371fb24f6d59b7aee062d32095baea948
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-12-06 13:02:12 -0500

    added player container

--- src/alert.js
+++ src/alert.js
@@ -1,21 +1,20 @@
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
+
 const MySwal = withReactContent(Swal);
 
-const alert = (nextRound, winner) => {
+const alert = (nextRound, winner,reset) => {
     
     MySwal.fire({
     
         onClose: () => {
             nextRound(2, "firstCards");
+            reset();
         },
         title: `What?`,
         text: `${winner} wins!`,
-        background: `rgba( 255, 255, 255, 0.9)`,
-        backdrop: `
-                #142b68`,
-        
+        background: `rgba( 255, 255, 255, 0.9)`,        
         showConfirmButton: true,
         confirmButtonText: 'Next round!',
     })
