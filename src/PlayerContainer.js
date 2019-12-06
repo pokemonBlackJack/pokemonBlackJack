906662a8fa4ba24f707414c857a12f33d932bfe7
tseki88 <teruhisaseki@gmail.com>
2019-12-06 11:26:33 -0500

    created playerContainer component

---
+++ src/PlayerContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+class PlayerContainer extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    };
+};
+
+export default PlayerContainer;
