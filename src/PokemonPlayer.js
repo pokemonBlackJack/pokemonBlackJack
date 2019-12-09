e9ac45292591bbd750c525c26c351724610ec85a
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-12-09 12:09:22 -0500

    changed the position of card element

--- src/PokemonPlayer.js
+++ src/PokemonPlayer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PlayerContainer from "./PlayerContainer";
 
 
  class PokemonPlayer extends Component {
@@ -8,14 +9,19 @@ import React, { Component } from 'react';
 				
 				    {this.props.getPokemon.map((pokemon, index) => {
 						return <div key={pokemon.id} className="playerPokemonContainer">
-					
-							<p>{pokemon.firstPokemon}</p>
-							<img className="pokemon" src={pokemon.firstPokemonImg} alt={`a ${pokemon.firstPokemon} ready to battle`} />
-							<span className="imageShadow"></span>
-							<div className={`healthBar`}>
-								<div className={index === 0 ? (
-									this.props[`player2Score`] === 1 ? "halfHealth" : this.props[`player2Score`] === 2 ? "noHealth" : "fullHealth") : (
-										this.props[`player1Score`] === 1 ? "halfHealth" : this.props[`player1Score`] === 2 ? "noHealth" : "fullHealth") }></div>
+							<PlayerContainer cards={this.props[`player${index + 1}Cards`]} player={`Player ${index + 1}`} score={this.props[`player${index + 1}Score`]} flipable={(this.props.currentPlayer === index + 1 && !this.props.hideCards) || this.props.showAll ? true : false} cleanBoard={this.props.cleanBoard} />
+							<div className="playerPokemonDiv">
+								<p>{pokemon.firstPokemon}</p>
+								<img className="pokemon" src={pokemon.firstPokemonImg} alt={`a ${pokemon.firstPokemon} ready to battle`} />
+								<span className="imageShadow"></span>
+								<div className={`healthBar`}>
+									<div className={index === 0 ? (
+										this.props[`player2Score`] === 1 ? "halfHealth" : this.props[`player2Score`] === 2 ? "noHealth" : "fullHealth") : (
+											this.props[`player1Score`] === 1 ? "halfHealth" : this.props[`player1Score`] === 2 ? "noHealth" : "fullHealth") }>
+
+									</div>
+								</div>
+
 							</div>
          			 	</div>
        			 })}
