24281fb0db5110800759d3e765bc0ba56a5aefb6
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-12-09 21:12:33 -0500

    disable buttons and made faint animation

--- src/PokemonPlayer.js
+++ src/PokemonPlayer.js
@@ -12,12 +12,13 @@ import PlayerContainer from "./PlayerContainer";
 							<PlayerContainer cards={this.props[`player${index + 1}Cards`]} player={`Player ${index + 1}`} score={this.props[`player${index + 1}Score`]} flipable={(this.props.currentPlayer === (index + 1) && !this.props.hideCards) || this.props.showAll ? true : false} cleanBoard={this.props.cleanBoard} />
 							<div className="playerPokemonDiv">
 								<p>{pokemon.firstPokemon}</p>
-								<img className="pokemon" src={pokemon.firstPokemonImg} alt={`a ${pokemon.firstPokemon} ready to battle`} />
+								<img className={`pokemon ${this.props.winner !== null && this.props.winner !== (index + 1) ?  "faint" : ""}`} src={pokemon.firstPokemonImg} alt={`a ${pokemon.firstPokemon} ready to battle`} />
 								<span className="imageShadow"></span>
 								<div className={`healthBar`}>
 									<div className={index === 0 ? (
 										this.props[`player2Score`] === 1 ? "halfHealth" : this.props[`player2Score`] === 2 ? "noHealth" : "fullHealth") : (
 											this.props[`player1Score`] === 1 ? "halfHealth" : this.props[`player1Score`] === 2 ? "noHealth" : "fullHealth") }>
+												
 
 									</div>
 								</div>
